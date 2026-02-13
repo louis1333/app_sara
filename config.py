b15e882db7bebb285ee1f53a3acb73f330564a78
Diana <dsantamariat04@gmail.com>
2026-02-12 20:15:47 -0500

    Base de datos

--- config.py
+++ config.py
@@ -4,5 +4,11 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = 'dev-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql://postgres:Louis.alejo133@"
+        "db.qcxnouurjgspgqkrfvoh.supabase.co:5432/postgres"
+        "?sslmode=require"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
